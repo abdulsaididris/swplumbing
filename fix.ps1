9e2a0e0fca3f1a71869c308164433c139cc7d89f
Abdul Said <a.said@lagroupofcompanies.ca>
2026-05-17 16:46:44 -0600

    Fixed character encoding bugs project-wide

--- fix.ps1
+++
@@ -1,9 +0,0 @@
-$files = Get-ChildItem -Path . -Filter *.html -Recurse
-foreach ($file in $files) {
-    $content = [System.IO.File]::ReadAllText($file.FullName, [System.Text.Encoding]::UTF8)
-    $newContent = $content.Replace("â€”", "—").Replace("â€“", "–").Replace("â€™", "’").Replace("â€œ", "“").Replace("â€ ", "”")
-    if ($content -cne $newContent) {
-        [System.IO.File]::WriteAllText($file.FullName, $newContent, [System.Text.Encoding]::UTF8)
-        Write-Host "Updated $($file.Name)"
-    }
-}
